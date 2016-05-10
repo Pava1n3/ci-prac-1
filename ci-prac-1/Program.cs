10e94d477debbcd71cc12cc68edee6ceabac12b8
Dutchman97 <makatipu97@hotmail.com>
2016-05-10 15:13:16 +0200

    Things that work in theory

--- ci-prac-1/Program.cs
+++ ci-prac-1/Program.cs
@@ -1,9 +1,91 @@
-﻿namespace ci_prac_1
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ci_prac_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StreamReader stream = File.OpenText(args[0]);
+            string line = stream.ReadLine();
+            string[] lineChars = line.Split(' ');
+            int length = lineChars.Length;
+
+            int[,] sudoku = new int[length, length];
+            for (int x = 0; x < length; x++)
+                sudoku[x, 0] = int.Parse(lineChars[x]);
+            int y = 1;
+
+            while (!stream.EndOfStream)
+            {
+                line = stream.ReadLine();
+                lineChars = line.Split(' ');
+                for (int x = 0; x < length; x++)
+                    sudoku[x, y] = int.Parse(lineChars[x]);
+                y++;
+            }
+
+            Console.WriteLine();
+            for (y = 0; y < length; y++)
+            {
+                for (int i = 0; i < length; i++)
+                    Console.Write(sudoku[x, y].ToString() + " ");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static Sudoku NextSuccessor(Sudoku sudoku)
+        {
+            // Loop through all of the tiles in the field
+            for (int y = 0; y < sudoku.Field.GetLength(0); y++)
+                for (int x = 0; x < sudoku.Field.GetLength(0); x++)
+                    // If the tile is empty
+                    if (sudoku.Field[x, y] == 0)
+                    {
+                        // Get the numbers that can validly be put in the tile
+                        int[] validNumbers = GetValidNumbers(sudoku.Field, x, y);
+
+                        for (int i = 0; i < validNumbers.Length; i++)
+                            // Check if we have already expanded this successor
+                            if (!sudoku.ExpandedSuccessors[x, y][validNumbers[i] - 1])
+                            {
+                                Sudoku successor = new Sudoku(sudoku.Field);
+                                successor.Field[x, y] = validNumbers[i];
+                                sudoku.ExpandedSuccessors[x, y][validNumbers[i] - 1] = true;
+                                return successor;
+                            }
+                    }
+
+            return null;
+        }
+
+        static int[] GetValidNumbers(int[,] sudoku, int x, int y)
+        {
+            int[] invalid = new int[sudoku.GetLength(0) + 1];
+            for (int x1 = 0; x1 < sudoku.GetLength(0); x1++)
+                invalid[sudoku[x1, y]]++;
+
+            for (int y1 = 0; y1 < sudoku.GetLength(0); y1++)
+                invalid[sudoku[x, y1]]++;
+
+            // x2, y2 = index of square
+            int root = (int)Math.Sqrt(sudoku.GetLength(0));
+            int x2 = x - x % root;
+            int y2 = y / root;
+
+            for (int y3 = y2; y3 < y2 + root; y3++)
+                for (int x3 = x2; x3 < x2 + root; x3++)
+                    invalid[sudoku[x3, y3]]++;
+
+            List<int> valid = new List<int>();
+            for (int i = 0; i < invalid.Length; i++)
+                if (invalid[i] == 0)
+                    valid.Add(i);
+
+            return valid.ToArray();
         }
     }
 }
---
+++ ci-prac-1/Sudoku.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ci_prac_1
+{
+    public class Sudoku
+    {
+        // Whole sudoku = Field
+        // Group of nxn tiles = Square
+        // One individual tile = Tile
+
+        public Sudoku(int[,] sudoku)
+        {
+            this.Field = sudoku;
+            this.ExpandedSuccessors = new bool[sudoku.GetLength(0), sudoku.GetLength(0)][];
+            for (int y = 0; y < sudoku.GetLength(0); y++)
+                for (int x = 0; x < sudoku.GetLength(0); x++)
+                    this.ExpandedSuccessors[x, y] = new bool[sudoku.GetLength(0)];
+        }
+
+        public int[,] Field;
+        public bool[,][] ExpandedSuccessors;
+    }
+}
